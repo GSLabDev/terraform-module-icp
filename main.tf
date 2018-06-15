da6f25976d56924eb3d3ee528c5277ffc4fb3e7a
Ganesh C. GitHub <chokhareganesh@outlook.com>
2018-06-15 16:21:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ module "basic_topo" {
   source = "modules/basic_topology/"
   default_worker="1"
   extra_worker= "1" 
-  worker_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
-  master_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  worker_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
+  master_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
   ssh_private_key_path = "/work/demo/module_demo_basic/test/basic/id_rsa" 
 }
 
