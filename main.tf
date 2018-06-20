4e936dedff6723b7eaabadf52866efaf2bfc41a0
Ganesh C. GitHub <chokhareganesh@outlook.com>
2018-06-20 17:29:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,23 +5,23 @@ uri = "qemu:///system"
 
 #importing all modues from module directory
 module "distributed_topo" {
-  
-  source = "modules/distributed_topology/"
+
+  #For Basic only master_img_path and worker_img_path is required.
   #source = "modules/basic_topology/"
-  default_worker="0"
-  extra_worker= "1" 
-<<<<<<< HEAD
-  #worker_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
-  #master_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
-  #proxy_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
- # mng_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
- # boot_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
-  ssh_private_key_path = "/work/demo/module_demo_basic/test/basic/terraform-provider-icp/id_rsa" 
-=======
+  #worker_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
+  #master_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  
+  #For Distributed all node images path required.
+  source = "modules/distributed_topology/"
+  proxy_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  mng_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  boot_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
   worker_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
   master_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
-  ssh_private_key_path = "/work/demo/module_demo_basic/test/basic/id_rsa" 
->>>>>>> d7cd5049d0e4307c2784e64a98fbce4fa68316d4
+  
+  ssh_private_key_path = "/work/demo/module_demo_basic/test/basic/id_rsa"
+  default_worker="1"
+  extra_worker= "0"
 }
 
 
