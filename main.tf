ec237a744b0522a8dd95515747fb5e437d546fce
Ganesh C. GitHub <chokhareganesh@outlook.com>
2018-06-20 17:40:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,18 +8,19 @@ module "distributed_topo" {
 
   #For Basic only master_img_path and worker_img_path is required.
   #source = "modules/basic_topology/"
-  #worker_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
-  #master_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  #worker_img_path ="<paste_worker_image_path>" 
+  #master_img_path ="<paste_master_image_path>"
   
   #For Distributed all node images path required.
   source = "modules/distributed_topology/"
-  proxy_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
-  mng_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
-  boot_img_path ="/home/ganesh/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
-  worker_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img" 
-  master_img_path ="/home/user/Downloads/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  proxy_img_path ="<paste_proxy_image_path>"
+  mng_img_path ="<paste_management_image_path>"
+  boot_img_path ="<paste_boot_image_path>" 
+  worker_img_path ="<paste_worker_image_path>" 
+  master_img_path ="<paste_master_image_path>"
   
-  ssh_private_key_path = "/work/demo/module_demo_basic/test/basic/id_rsa"
+  #common parameter for basic and distributed topology 
+  ssh_private_key_path = "/root/.ssh/id_rsa"
   default_worker="1"
   extra_worker= "0"
 }
