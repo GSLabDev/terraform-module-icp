962b71832e13dd701fd92e2cad8c974b43cc4ea9
chokhareganesh <chokhareganesh@outlook.com>
2018-06-21 17:13:32 +0530

    After removing < > symbols from main.tf

--- main.tf
+++ main.tf
@@ -8,19 +8,19 @@ module "distributed_topo" {
 
   #For Basic only master_img_path and worker_img_path is required.
   #source = "modules/basic_topology/"
-  #worker_img_path ="<paste_worker_image_path>" 
-  #master_img_path ="<paste_master_image_path>"
+  #worker_img_path ="paste_worker_image_path" 
+  #master_img_path ="paste_master_image_path"
   
   #For Distributed all node images path required.
   source = "modules/distributed_topology/"
-  proxy_img_path ="<paste_proxy_image_path>"
-  mng_img_path ="<paste_management_image_path>"
-  boot_img_path ="<paste_boot_image_path>" 
-  worker_img_path ="<paste_worker_image_path>" 
-  master_img_path ="<paste_master_image_path>"
+  proxy_img_path ="paste_proxy_image_path"
+  mng_img_path ="paste_management_image_path"
+  boot_img_path ="paste_boot_image_path" 
+  worker_img_path ="paste_worker_image_path" 
+  master_img_path ="paste_master_image_path"
   
   #common parameter for basic and distributed topology 
-  ssh_private_key_path = "<paste_path_of_id_rsa>"
+  ssh_private_key_path = "paste_path_of_id_rsa"
   default_worker="1"
   extra_worker= "0"
 }
